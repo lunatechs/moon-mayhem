6f761c00551bffa334d7bde0b3955e190d3613d4
Morgan Nomura <morganelle@gmail.com>
2017-02-28 21:27:02 -0800

    slight refactoring of functions to get data ready for charts. deleted fakeData because working with real data is performant

--- public/scripts/models/moon.js
+++ public/scripts/models/moon.js
@@ -2,7 +2,9 @@
 
 (function(module) {
   var moonData = {};
+  var dataContent;
 
+  // Filter function for matching month and year vars from form
   function filterFormData(value) {
     if (value.date.slice(0, 2) === moonController.month) {
       if(value.date.slice(6, 10) === moonController.year) {
@@ -11,29 +13,35 @@
     }
   }
 
+  // Sort function to arrange days in ascending order
+  function sortDays(days) {
+    days.sort((a,b) => (a) - (b));
+  }
+
+  // JSON call to get filtered data
   moonData.getData = function() {
-    console.log('get data called');
-    $.getJSON('../../data/fakeData.json') // switch to real json at some point!
-    .then(jsonData => moonData.dataContent = jsonData.filter(filterFormData));
+    $.getJSON('../../data/cityData.json')
+    .then(jsonData => dataContent = jsonData.filter(filterFormData));
   };
 
+  // Yields an array with a unique date within a month
   moonData.getDays = function() {
-    moonData.days = moonData.dataContent.map(datum => datum.date.slice(3,5))
-      .reduce(function(a,b){
+    moonData.days = dataContent.map(datum => datum.date.slice(3,5))
+      .reduce(function(a,b) {
         if (a.indexOf(b) < 0 ) a.push(b);
         return a;
       },[]);
+      sortDays(moonData.days);
   }
 
+  // Yields an array with crime counts for each day
   moonData.getCounts = function() {
-    moonData.counts = [];
-    moonData.days.forEach(function(day) {
-      moonData.counts.push(
-        moonData.dataContent.filter(function(datum) {
-            if (datum.date.slice(3,5) === day) {
-              return datum;
-            }
-          }).length);
+    moonData.counts = moonData.days.map(function(day) {
+      return dataContent.filter(function(datum) {
+        if (datum.date.slice(3,5) === day) {
+          return datum;
+        }
+      }).length;
     });
   };
 
