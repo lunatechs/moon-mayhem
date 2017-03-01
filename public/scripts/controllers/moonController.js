1ba529a60a82013d8641c4758c49672797e48d8e
Morgan Nomura <morganelle@gmail.com>
2017-02-28 17:08:22 -0800

    connect form submission with getting and filtering data from JSON

--- public/scripts/controllers/moonController.js
+++ public/scripts/controllers/moonController.js
@@ -7,7 +7,9 @@
 
 $('#moon-selector').on('submit', function(e) {
   e.preventDefault();
-  console.log('hi');
+  moonController.month = $('select')[0].value;
+  moonController.year = $('select')[1].value;
+  moonData.getData(); // once form is submitted, call getData
 })
   moonController.index = function() {
     console.log('moon contoller cawled');
--- public/scripts/models/moon.js
+++ public/scripts/models/moon.js
@@ -1,1 +1,19 @@
-'use strict'
+'use strict';
+
+var moonData = {};
+
+moonData.filterData = function(value) {
+  console.log('form inputs in filter data',moonController.month, moonController.year);
+  if (value.date.slice(0, 2) === moonController.month) {
+    if(value.date.slice(6, 10) === moonController.year) {
+      return value;
+    }
+  }
+}
+
+moonData.getData = function() {
+  $.getJSON('../../data/fakeData.json')
+  .then(function(jsonData) {
+    moonData.dataContent = jsonData.filter(moonData.filterData);
+  });
+}
