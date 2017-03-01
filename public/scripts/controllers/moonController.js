84fac10358c992840d01b64d37237f8105debb34
Morgan Nomura <morganelle@gmail.com>
2017-02-28 20:50:05 -0800

    extracting chart data

--- public/scripts/controllers/moonController.js
+++ public/scripts/controllers/moonController.js
@@ -5,14 +5,14 @@
   $('section').hide();
   $('section#main-moon').show();
 
-$('#moon-selector').on('submit', function(e) {
-  e.preventDefault();
-  moonController.month = $('select')[0].value;
-  moonController.year = $('select')[1].value;
-  moonData.getData(); // once form is submitted, call getData
-})
+  $('#moon-selector').on('submit', function(e) {
+    e.preventDefault();
+    moonController.month = $('select')[0].value;
+    moonController.year = $('select')[1].value;
+    moonData.getData(); // may want to move this once view functions are in place
+  });
   moonController.index = function() {
-    console.log('moon contoller cawled');
+    console.log('moon controller called');
     $('section').hide();
     $('section#main-moon').show();
   }
--- public/scripts/models/moon.js
+++ public/scripts/models/moon.js
@@ -1,23 +1,41 @@
 'use strict';
 
-var moonData = {};
+(function(module) {
+  var moonData = {};
 
-moonData.filterData = function(value) {
-  console.log('form inputs in filter data',moonController.month, moonController.year);
-  if (value.date.slice(0, 2) === moonController.month) {
-    if(value.date.slice(6, 10) === moonController.year) {
-      return value;
+  function filterFormData(value) {
+    if (value.date.slice(0, 2) === moonController.month) {
+      if(value.date.slice(6, 10) === moonController.year) {
+        return value;
+      }
     }
   }
-}
 
-moonData.getData = function() {
-  $.getJSON('../../data/fakeData.json') // switch to real json at some point!
-  .then(function(jsonData) {
-    moonData.dataContent = jsonData.filter(moonData.filterData);
-    moonData.dayCounts = moonData.dataContent.map(function(datum) {
-      //console.log(datum);
-      return datum.date.slice(3,5);
+  moonData.getData = function() {
+    console.log('get data called');
+    $.getJSON('../../data/fakeData.json') // switch to real json at some point!
+    .then(jsonData => moonData.dataContent = jsonData.filter(filterFormData));
+  };
+
+  moonData.getDays = function() {
+    moonData.days = moonData.dataContent.map(datum => datum.date.slice(3,5))
+      .reduce(function(a,b){
+        if (a.indexOf(b) < 0 ) a.push(b);
+        return a;
+      },[]);
+  }
+
+  moonData.getCounts = function() {
+    moonData.counts = [];
+    moonData.days.forEach(function(day) {
+      moonData.counts.push(
+        moonData.dataContent.filter(function(datum) {
+            if (datum.date.slice(3,5) === day) {
+              return datum;
+            }
+          }).length);
     });
-  });
-}
+  };
+
+  module.moonData = moonData;
+})(window);
